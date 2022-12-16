b369e2394c1db98527feac1a3ae28369b589d2e6
Sheena Bonilla <sheena.myb@gmail.com>
2022-12-15 19:26:43 -0800

    Install react bootstrap and modify nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Router from './components/Router';
+import Nav from './components/nav/Nav';
 
 const App = () => {
   return (
     <div className="App">
+      <Nav />
       <Router />
     </div>
   )
