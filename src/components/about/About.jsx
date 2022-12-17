9d8c5dccbc9a2764ae076c185c2ce9e2e2bec711
Sheena Bonilla <sheena.myb@gmail.com>
2022-12-16 20:14:17 -0800

    Modify about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const About = () => {
   return (
     <section>
-        <h2>About Me</h2>
+        <h1>About Me</h1>
+        <h5>Specialties: Buyer's agent, Listing agent, Relocation, Legal, Landlord</h5>
+        <div>
+          <p>
+            I became a real estate agent because I want to help people make informed decisions regarding
+            their property--whether it's buying or selling. My purpose in life has always been to serve
+            others. As an attorney, I help clients navigate through the legal system. As an adjunct professor,
+            I help students prepare for the "real world." Regardless of what hat I'm wearing, the most important
+            thing to me is helping others succeed and reach their goals. I strive to provide the best service
+            so that you can have the best experience.
+          </p>
+        </div>
     </section>
   )
 }
