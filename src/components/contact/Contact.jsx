e5156731857c48e708082f792231755153b2360f
Sheena Bonilla <sheena.myb@gmail.com>
2022-12-17 20:35:59 -0800

    Add contact info and socials

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <section>
+    <section id="contact">
         <h2>Contact Me</h2>
         <div>
           <p>324 S. Beverly Dr. Suite 490 <br/>
                Beverly Hills, CA 90212
           </p>
+        </div>
+        <div className="contact_options">
+          <article className="contact_option">
+            <h4>Email</h4>
+            <a href="mailto:macaraeg.april@gmail.com" target="_blank">Send an email</a>
+          </article>
+          <article className="contact_option">
+            <h4>Phone</h4>
+            <a href="tel:4242614316" target="_blank">Phone</a>
+          </article>
+          <article className="contact_option">
+            <h4>Instagram</h4>
+            <a href="https://www.instagram.com/aprilrealty/" target="_blank">Follow me on Instagram</a>
+          </article>
+          
         </div>
     </section>
   )
