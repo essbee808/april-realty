a3ed2f98536db40a48c140b100848e1e76123352
Sheena Bonilla <sheena.myb@gmail.com>
2022-12-20 20:42:02 -0800

    Import footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBarContainer from './components/nav/NavBarContainer';
 import Router from './components/Router';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
     <div className="App">
       <NavBarContainer/>
       <Router />
+      <Footer />
     </div>
   )
 }
