702088c920630d7fee9d84f3b1742b447846b605
Sheena Bonilla <sheena.myb@gmail.com>
2022-12-21 20:45:09 -0800

    Import social icon

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {FiInstagram} from 'react-icons/fi'
 
 const Footer = () => {
   return (
     <footer>
       <div className="footer_socials">
-        <a href="https://www.instagram.com/aprilrealty" target="_blank">Instagram</a>
+        <a href="https://www.instagram.com/aprilrealty" target="_blank"><FiInstagram/></a>
         <a href=""></a>
         
       </div>
