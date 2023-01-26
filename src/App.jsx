3c9b482883aa595b5c271ede3e898c5557345f2c
Sheena Bonilla <sheena.myb@gmail.com>
2023-01-25 22:16:09 -0800

    Import useNavigate hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import NavBarContainer from './components/nav/NavBarContainer';
+import {useNavigate} from 'react-router-dom';
 import Router from './components/Router';
 import Footer from './components/footer/Footer';
 
 const App = () => {
+  const navigate = useNavigate();
+
+  const navigateToContact = () => {
+    navigate('/contact');
+  }
+
   return (
     <div className="App">
       <NavBarContainer/>
       <Router />
-      <button className="material-icons floating-btn">mail</button>
+      <button className="material-icons floating-btn" onClick={navigateToContact}>mail</button>
       <Footer />
     </div>
   )
