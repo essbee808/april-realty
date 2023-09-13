cb9f2f214fe64f1d27237af0c87b101470a9ca56
Sheena Bonilla <sheena.myb@gmail.com>
2023-09-13 12:32:57 -0700

    Upload new promo video

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,9 +8,14 @@ const Home = () => {
     <section>
       <Container>
           <Row>
-              <div className="aspect-w-16 aspect-h-9">
+              {/* <div className="aspect-w-16 aspect-h-9">
                 <iframe src="https://www.youtube.com/embed/6puBdEOW7Dg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div> */}
+
+              <div className="aspect-w-16 aspect-h-9">
+                <iframe src="https://player.vimeo.com/video/864134934?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" title="Grate Real Estate Promo Video"></iframe>
               </div>
+              <script src="https://player.vimeo.com/api/player.js"></script>
           </Row>
           <Row>
               <h2>Featured Neighborhoods</h2>
--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Reviews = () => {
   return (
     <section>
-        <h1 className="text-center">Reviews Coming Soon!</h1>
+        <h1 className="text-center">Reviews Coming Soon! :)</h1>
     </section>
   )
 }
