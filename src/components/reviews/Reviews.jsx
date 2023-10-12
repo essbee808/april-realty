140bce37d31e9967690e467a3f239c047dfc9595
Sheena Bonilla <sheena.myb@gmail.com>
2023-10-12 14:00:13 -0700

    Modify media queries for reviews carousel

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -14,6 +14,35 @@ const Reviews = () => {
       <h1 className="main-heading text-center">Testimonials</h1>
       <Carousel>
         <Carousel.Item>
+         <Image src={BG} text="Second Slide"></Image>
+         <Carousel.Caption>
+            {/* <h2 className="slide-text">First Slide</h2> */}
+            <p>April is hands down the best realtor! She went above and beyond! We would not have gotten the house we 
+              wanted had it not been for her. She will make you feel heard and understood while at the same time 
+              offering her expertise and guidance. April uses her knowledge of real estate and negotiation skills 
+              as a lawyer to fiercely advocate for her clients. Above all, she is genuinely a good person. She will 
+              treat you more like family than a client, and you will see very early on, she has your best interest 
+              at heart. Having someone like her in your corner during such a life changing process is priceless. 
+              Thank you, April!</p>
+            <p>- K.B.</p>
+          </Carousel.Caption>
+        </Carousel.Item>  
+
+        <Carousel.Item>
+         <Image src={BG} text="Second Slide"></Image>
+         <Carousel.Caption>
+            {/* <h2 className="slide-text">First Slide</h2> */}
+            <p>We are so glad to have April as our agent for the purchase of our home. She made the process a lot less 
+              stressful. She is very knowledgeable, communicative and understanding as we got cold feet a couple of 
+              times during the process. She made sure we understood all the paperwork and that we were on-time to meet 
+              the deadlines. She was supportive and provided us all the information we need to make a decision confidently 
+              and independently.</p>
+            <p>We definitely recommend April’s services to anyone looking to buy or sell!</p>
+            <p>- L.</p>
+          </Carousel.Caption>
+        </Carousel.Item>  
+
+        <Carousel.Item>
          <Image src={BG} text="First Slide"></Image>
          <Carousel.Caption>
             {/* <h2 className="slide-text">First Slide</h2> */}
@@ -26,6 +55,8 @@ const Reviews = () => {
             <p>- N.L.</p>
           </Carousel.Caption>
         </Carousel.Item>
+
+        
         {/* <Carousel.Item>
           <Image src={Review1} text="First Slide"/>
            <Carousel.Caption>
