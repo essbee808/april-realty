bcb8e7c7b4a9292821ad220448d92f01a5883048
Sheena Bonilla <sheena.myb@gmail.com>
2023-10-17 10:16:02 -0700

    Test carousel for responsiveness

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -17,7 +17,7 @@ const Reviews = () => {
          <Image src={BG} text="First Slide"></Image>
          <Carousel.Caption>
             {/* <h2 className="slide-text">First Slide</h2> */}
-            <p>Having someone like her in your corner during such a life changing process is priceless. 
+            <p>"Having someone like her in your corner during such a life changing process is priceless." 
               </p>
             <p>- K.B.</p>
           </Carousel.Caption>
@@ -27,9 +27,9 @@ const Reviews = () => {
          <Image src={BG} text="Second Slide"></Image>
          <Carousel.Caption>
             {/* <h2 className="slide-text">First Slide</h2> */}
-            <p>April is very knowledgeable, communicative and understanding as we got cold feet a couple of 
+            <p>"April is very knowledgeable, communicative and understanding as we got cold feet a couple of 
               times during the process. She made sure we understood all the paperwork and that we were on-time to meet 
-              the deadlines.</p>
+              the deadlines."</p>
             <p>- L.</p>
           </Carousel.Caption>
         </Carousel.Item>  
@@ -38,8 +38,8 @@ const Reviews = () => {
          <Image src={BG} text="Third Slide"></Image>
          <Carousel.Caption>
             {/* <h2 className="slide-text">First Slide</h2> */}
-            <p>What sets April  apart from ordinary realtors is April's impeccable business acumen and her effortless ability to cultivate 
-              relationships with agents, sellers, neighbors, mortgage lenders, and people alike.</p>
+            <p>"What sets April  apart from ordinary realtors is April's impeccable business acumen and her effortless ability to cultivate 
+              relationships with agents, sellers, neighbors, mortgage lenders, and people alike."</p>
             <p>- N.L.</p>
           </Carousel.Caption>
         </Carousel.Item>
@@ -48,7 +48,7 @@ const Reviews = () => {
          <Image src={BG} text="Fourth Slide"></Image>
          <Carousel.Caption>
             {/* <h2 className="slide-text">First Slide</h2> */}
-            <p>She's fast and quick to respond. Friendly, knowledgeable and professional.  Highly recommend April!</p>
+            <p>"She's fast and quick to respond. Friendly, knowledgeable and professional. Highly recommend April!"</p>
             <p>- L.M.</p>
           </Carousel.Caption>
         </Carousel.Item>
@@ -57,8 +57,8 @@ const Reviews = () => {
          <Image src={BG} text="Fifth Slide"></Image>
          <Carousel.Caption>
             {/* <h2 className="slide-text">First Slide</h2> */}
-            <p>April wasn’t only great with me, she was amazing and great to everyone we came in contact with, making the experience 
-              so pleasant and positive.</p>
+            <p>"April wasn’t only great with me, she was amazing and great to everyone we came in contact with, making the experience 
+              so pleasant and positive."</p>
             <p>- C.P.</p>
           </Carousel.Caption>
         </Carousel.Item>
@@ -67,7 +67,7 @@ const Reviews = () => {
          <Image src={BG} text="Sixth Slide"></Image>
          <Carousel.Caption>
             {/* <h2 className="slide-text">First Slide</h2> */}
-            <p>April was absolutely amazing. She wasn’t just our realtor, she quickly became our friend.</p>
+            <p>"April was absolutely amazing. She wasn’t just our realtor, she quickly became our friend."</p>
             <p>- M.H.</p>
           </Carousel.Caption>
         </Carousel.Item>
